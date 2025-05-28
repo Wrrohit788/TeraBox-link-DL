f40b6b3a79b92340dc7471e8d5c283e8dc223d24
Wrrohit788 <154300370+Wrrohit788@users.noreply.github.com>
2025-05-28 21:11:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if (!token) {
 }
 
 const bot = new TelegramBot(token, { polling: true });
-const updatesChannel = '@Opleech_WD';
+const updatesChannel = '@motumovies';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -66,7 +66,7 @@ const sendStartMessage = (chatId) => {
         parse_mode: 'Markdown',
         reply_markup: {
             inline_keyboard: [
-                [{ text: '〇 𝐉𝐨𝐢𝐧 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐓𝐨 𝐔𝐬𝐞 𝐌𝐞 〇', url: 'https://t.me/Opleech_WD' }],
+                [{ text: '〇 𝐉𝐨𝐢𝐧 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐓𝐨 𝐔𝐬𝐞 𝐌𝐞 〇', url: 'https://t.me/wrrohit02' }],
                 [{ text: '🔗 How to use Bot 🔗', url: 'https://t.me/WOODcraft_Mirror_Zone/43' }]
             ]
         }
